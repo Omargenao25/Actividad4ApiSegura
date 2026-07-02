fb733c324b9954be4656b07b9ca55532c3f43abb
Omargenao25 <omargenao78@gmail.com>
2026-07-02 00:09:28 -0400

    avances pertinentes, creacion de modelos y contexto aparte del conecction strings.

---
+++ BibliotecaDbContext/BibliotecaDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Actividad4ApiSegura.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad4ApiSegura.BibliotecaDbContext;
+
+public partial class BibliotecaDbContext : DbContext
+{
+    public BibliotecaDbContext()
+    {
+    }
+
+    public BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Autor> Autors { get; set; }
+
+    public virtual DbSet<Libro> Libros { get; set; }
+
+  
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Autor>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Autor__3214EC073EF60762");
+        });
+
+        modelBuilder.Entity<Libro>(entity =>
+        {
+            entity.HasOne(d => d.Autor).WithMany(p => p.Libros)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Libros_Autor");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Controllers/LibrosController.cs
@@ -0,0 +1,94 @@
+﻿
+using Actividad4ApiSegura.BibliotecaDbContext;
+using Actividad4ApiSegura.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad3ApiDB.Controllers
+{
+    [Route("api/libros")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly BibliotecaDbContext _context;
+
+        public LibrosController(BibliotecaDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libro>>> GetLibros([FromQuery] int page = 1, [FromQuery] int pageSize = 5)
+        {
+            if (page < 1) page = 1;
+            if (pageSize < 1) pageSize = 5;
+
+            var libros = await _context.Libros
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return Ok(libros);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Libro>> GetLibro(int id)
+        {
+            var libro = await _context.Libros.FindAsync(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(libro);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Libro>> PostLibro(Libro libro)
+        {
+            _context.Libros.Add(libro);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetLibro), new { id = libro.Id }, libro);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLibro(int id, Libro libro)
+        {
+            if (id != libro.Id)
+            {
+                return BadRequest();
+            }
+
+            var libroExiste = await _context.Libros.AnyAsync(l => l.Id == id);
+            if (!libroExiste)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(libro).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLibro(int id)
+        {
+            var libro = await _context.Libros.FindAsync(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libros.Remove(libro);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
